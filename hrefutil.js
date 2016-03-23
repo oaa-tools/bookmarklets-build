ce898a73c8f1e89e7fbd53979df9119ddf2bae2c
Nicholas Hoyt <nhoyt@illinois.edu>
2016-03-23 10:54:14 -0500

    Add version logging

--- hrefutil.js
+++
@@ -1,9 +0,0 @@
-function createBookmarkletHref (name, url) {
-  var date = "160323";
-  var bookmarklet = "window.a11y" + name;
-  var scriptname = name.toLowerCase() + '.js';
-  var baseUrl = url.trim();
-  if (!baseUrl.endsWith('/')) baseUrl += '/';
-
-  return "javascript:/*" + date + "*/if(" + bookmarklet + ")" + bookmarklet + ".run();else{(function(){var%20link=document.createElement('link');link.rel='stylesheet';link.type='text/css';link.href='" + baseUrl + "build/styles.css';document.getElementsByTagName('head')[0].appendChild(link);var%20script=document.createElement('script');script.type='text/javascript';script.src='" + baseUrl + "build/" + scriptname + "';document.getElementsByTagName('head')[0].appendChild(script);})();}";
-}
